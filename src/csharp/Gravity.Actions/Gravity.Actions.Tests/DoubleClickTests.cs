5bf19c331cb8f72bfea72331d4432fadad9cff25
Gravity API <gravity.api@outlook.com>
2019-12-25 23:15:37 +0200

    DoubleClick implemented

---
+++ src/csharp/Gravity.Actions/Gravity.Actions.Tests/DoubleClickTests.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * CHANGE LOG - keep only last 5 threads
+ * 
+ * on-line resources
+ */
+using Gravity.Drivers.Mock.WebDriver;
+using Gravity.Services.ActionPlugins.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+#pragma warning disable S4144
+namespace Gravity.Services.ActionPlugins.Tests
+{
+    [TestClass]
+    public class DoubleClickTests : ActionTests
+    {
+        [DataTestMethod]
+        [DataRow("{'elementToActOn':'//positive'}")]
+        public void DoubleClickPositive(string actionRule)
+        {
+            // execute
+            ExecuteAction<DoubleClick>(actionRule);
+
+            // assertion (no assertion here, expected is no exception)
+            Assert.IsTrue(true);
+        }
+
+        [DataTestMethod, ExpectedException(typeof(WebDriverTimeoutException))]
+        [DataRow("{'elementToActOn':'//none'}")]
+        public void DoubleClickNoElement(string actionRule)
+        {
+            // execute
+            ExecuteAction<DoubleClick>(actionRule);
+
+            // assertion (no assertion here, expected WebDriverTimeoutException exception)
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void DoubleClickFlat()
+        {
+            // execute
+            ExecuteAction<DoubleClick>();
+
+            // assertion (no assertion here, expected is no exception)
+            Assert.IsTrue(true);
+        }
+
+        [DataTestMethod]
+        [DataRow("{'Argument':'{{$ --until:NoAlert}}','ElementToActOn':'//positive'}")]
+        public void DoubleClickUntilNoAlert(string actionRule)
+        {
+            // execute
+            ExecuteAction<DoubleClick>(actionRule, new Dictionary<string, object>
+            {
+                [MockCapabilities.HasAlert] = true
+            });
+
+            // assertion (no assertion here, expected is no exception)
+            Assert.IsTrue(true);
+        }
+
+        [DataTestMethod]
+        [DataRow("{'elementToActOn':'//positive'}")]
+        public void DoubleClickElementAbsolutePositive(string actionRule)
+        {
+            // execute
+            ExecuteAction<DoubleClick>(MockBy.Positive(), actionRule);
+
+            // assertion (no assertion here, expected is no exception)
+            Assert.IsTrue(true);
+        }
+
+        [DataTestMethod]
+        [DataRow("{'elementToActOn':'.//positive'}")]
+        public void DoubleClickElementRelativePositive(string actionRule)
+        {
+            // execute
+            ExecuteAction<DoubleClick>(MockBy.Positive(), actionRule);
+
+            // assertion (no assertion here, expected is no exception)
+            Assert.IsTrue(true);
+        }
+
+        [DataTestMethod, ExpectedException(typeof(WebDriverTimeoutException))]
+        [DataRow("{'elementToActOn':'//none'}")]
+        public void DoubleClickElementAbsoluteNoElement(string actionRule)
+        {
+            // execute
+            ExecuteAction<DoubleClick>(MockBy.Positive(), actionRule);
+
+            // assertion (no assertion here, expected is no exception)
+            Assert.IsTrue(true);
+        }
+
+        [DataTestMethod, ExpectedException(typeof(NoSuchElementException))]
+        [DataRow("{'elementToActOn':'.//none'}")]
+        public void DoubleClickElementRelativeNoElement(string actionRule)
+        {
+            // execute
+            ExecuteAction<DoubleClick>(MockBy.Positive(), actionRule);
+
+            // assertion (no assertion here, expected is no exception)
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void DoubleClickElementFlat()
+        {
+            // execute
+            ExecuteAction<DoubleClick>(MockBy.Positive());
+
+            // assertion (no assertion here, expected is no exception)
+            Assert.IsTrue(true);
+        }
+    }
+}
+#pragma warning restore S4144
--- src/csharp/Gravity.Actions/Gravity.Actions/Common/DoubleClick.cs
+++ src/csharp/Gravity.Actions/Gravity.Actions/Common/DoubleClick.cs
@@ -1,61 +1,45 @@
 ﻿/*
- * CHANGE LOG - keep only last 5 threads
+ * CHANGE LOG
  * 
  * 2019-01-03
- *    - modify: add support for click without specified element (flat action)
+ *    - modify: add support for double-click without specified element (flat action)
  *    - modify: improve XML comments
- *    - modify: change to JSON resource
  *    
  * 2019-01-11
- *    - modify: override action-name using ActionType constant
- *    - modify: improve element-level action
- *    -    fix: on element action always takes absolute XPath
- *    
- * 2019-08-22
- *    - modify: add support for special cases - for action only, NOT for extraction
+ *    - modify: override action-name using ActionType constant 
  * 
- * 2019-12-24
+ * 2019-12-25
  *    - modify: add constructor to override base class types
  * 
  * on-line resources
  */
 using Gravity.Drivers.Selenium;
 using Gravity.Services.Comet.Engine.Attributes;
-using Gravity.Services.Comet.Engine.Core;
 using Gravity.Services.Comet.Engine.Extensions;
 using Gravity.Services.Comet.Engine.Plugins;
 using Gravity.Services.DataContracts;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
 
 namespace Gravity.Services.ActionPlugins.Common
 {
     [Action(
         assmebly: "Gravity.Services.ActionPlugins, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
-        resource: "Gravity.Services.ActionPlugins.Documentation.click.json",
-        Name = ActionType.CLICK)]
-    public class Click : ActionPlugin
+        resource: "Gravity.Services.ActionPlugins.Documentation.double-click.json",
+        Name = ActionType.DOUBLE_CLICK)]
+    public class DoubleClick : ActionPlugin
     {
-        // constants: arguments
-        private const string UNTIL = "until";
-
         // members: state
         private readonly Actions actions;
-        private readonly WebDriverWait wait;
-        private IDictionary<string, string> arguments;
 
         /// <summary>
         /// Creates a new instance of this plug-in.
         /// </summary>
         /// <param name="webDriver">WebDriver implementation by which to execute the action.</param>
         /// <param name="webAutomation">This WebAutomation object (the original object sent by the user).</param>
-        public Click(IWebDriver webDriver, WebAutomation webAutomation)
+        public DoubleClick(IWebDriver webDriver, WebAutomation webAutomation)
             : this(webDriver, webAutomation, Utilities.GetTypes())
         { }
 
@@ -65,11 +49,10 @@ namespace Gravity.Services.ActionPlugins.Common
         /// <param name="webDriver">WebDriver implementation by which to execute the action.</param>
         /// <param name="webAutomation">This WebAutomation object (the original object sent by the user).</param>
         /// <param name="types">Types from which to load plug-ins repositories</param>
-        public Click(IWebDriver webDriver, WebAutomation webAutomation, IEnumerable<Type> types)
+        public DoubleClick(IWebDriver webDriver, WebAutomation webAutomation, IEnumerable<Type> types)
             : base(webDriver, webAutomation, types)
         {
             actions = new Actions(webDriver);
-            wait = new WebDriverWait(webDriver, TimeSpan.FromMilliseconds(PageLoadTimeout));
         }
 
         /// <summary>
@@ -84,19 +67,12 @@ namespace Gravity.Services.ActionPlugins.Common
                 return;
             }
 
-            // execute special action
-            arguments = new CliFactory(actionRule.Argument).Parse();
-            if (arguments.ContainsKey(UNTIL))
-            {
-                SpecialActionFactory(actionRule);
-                return;
-            }
-
             // get locator
             var by = ByFactory.Get(actionRule.Locator, actionRule.ElementToActOn);
 
             // execute action
-            WebDriver.GetElement(by, TimeSpan.FromMilliseconds(ElementSearchTimeout)).Click();
+            var element = WebDriver.GetElement(by, TimeSpan.FromMilliseconds(ElementSearchTimeout));
+            actions.DoubleClick(element).Build().Perform();
         }
 
         /// <summary>
@@ -121,7 +97,7 @@ namespace Gravity.Services.ActionPlugins.Common
                 : webElement.FindElement(by);
 
             // execute action
-            element.Click();
+            actions.DoubleClick(element).Build().Perform();
         }
 
         // execute flat action
@@ -130,44 +106,5 @@ namespace Gravity.Services.ActionPlugins.Common
             actions.Click().Build().Perform();
             return true;
         });
-
-        // special actions factory
-        private void SpecialActionFactory(ActionRule actionRule)
-        {
-            // setup binding flags
-            const BindingFlags B = BindingFlags.Instance | BindingFlags.NonPublic;
-            const StringComparison C = StringComparison.OrdinalIgnoreCase;
-
-            // shortcuts
-            var u = arguments[UNTIL];
-
-            // get method
-            var methods = GetType().GetMethods(B).Where(i => i.GetCustomAttribute<DescriptionAttribute>() != null);
-            var method = methods.FirstOrDefault(i => i.GetCustomAttribute<DescriptionAttribute>().Description.Equals(u, C));
-
-            // invoke
-            method.Invoke(this, new object[] { actionRule });
-        }
-
-#pragma warning disable S1144, RCS1213, IDE0051
-        [Description(nameof(NoAlert))]
-        private void NoAlert(ActionRule actionRule)
-        {
-            // get locator
-            var by = ByFactory.Get(actionRule.Locator, actionRule.ElementToActOn);
-
-            // click until condition met or timeout reached
-            wait.Until(webDriver =>
-            {
-                webDriver.GetElement(by).Click();
-                if (webDriver.HasAlert())
-                {
-                    webDriver.SwitchTo().Alert().Dismiss();
-                    return null;
-                }
-                return webDriver;
-            });
-        }
-#pragma warning restore
     }
 }

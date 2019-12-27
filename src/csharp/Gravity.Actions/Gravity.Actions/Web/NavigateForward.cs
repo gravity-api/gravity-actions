40100c24e6c5cf05b1d01551fea6008e938a808f
Gravity API <gravity.api@outlook.com>
2019-12-27 12:01:11 +0200

    NavigateForward implemented

---
+++ src/csharp/Gravity.Actions/Gravity.Actions/Web/NavigateForward.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * CHANGE LOG - keep only last 5 threads
+ * 
+ * 2019-01-12
+ *    - modify: improve XML comments
+ *    - modify: override ActionName using ActionType constant
+ *    
+ * 2019-12-27
+ *    - modify: add constructor to override base class types
+ * 
+ * on-line resources
+ */
+using Gravity.Services.Comet.Engine.Attributes;
+using Gravity.Services.Comet.Engine.Extensions;
+using Gravity.Services.Comet.Engine.Plugins;
+using Gravity.Services.DataContracts;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+
+namespace Gravity.Services.ActionPlugins.Web
+{
+    [Action(
+        assmebly: "Gravity.Services.ActionPlugins, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+        resource: "Gravity.Services.ActionPlugins.Documentation.navigate-forward.json",
+        Name = ActionType.NAVIGATE_FORWARD)]
+    public class NavigateForward : ActionPlugin
+    {
+        /// <summary>
+        /// Creates a new instance of this plug-in.
+        /// </summary>
+        /// <param name="webDriver">WebDriver implementation by which to execute the action.</param>
+        /// <param name="webAutomation">This WebAutomation object (the original object sent by the user).</param>
+        public NavigateForward(IWebDriver webDriver, WebAutomation webAutomation)
+            : this(webDriver, webAutomation, Utilities.GetTypes())
+        { }
+
+        /// <summary>
+        /// Creates a new instance of this plug-in.
+        /// </summary>
+        /// <param name="webDriver">WebDriver implementation by which to execute the action.</param>
+        /// <param name="webAutomation">This WebAutomation object (the original object sent by the user).</param>
+        /// <param name="types">Types from which to load plug-ins repositories.</param>
+        public NavigateForward(IWebDriver webDriver, WebAutomation webAutomation, IEnumerable<Type> types)
+            : base(webDriver, webAutomation, types)
+        { }
+
+        /// <summary>
+        /// Move back a single entry in the browser's history.
+        /// The action will be completed when page readyState='complete' or until page loading timeout reached.
+        /// </summary>
+        /// <param name="actionRule">This ActionRule instance (the original object sent by the user).</param>
+        public override void OnPerform(ActionRule actionRule)
+        {
+            Navigate(actionRule);
+        }
+
+        /// <summary>
+        /// Move back a single entry in the browser's history.
+        /// The action will be completed when page readyState='complete' or until page loading timeout reached.
+        /// </summary>
+        /// <param name="webElement">This WebElement instance on which to perform the action (provided by the extraction rule).</param>
+        /// <param name="actionRule">This ActionRule instance (the original object send by the user).</param>
+        public override void OnPerform(IWebElement webElement, ActionRule actionRule)
+        {
+            Navigate(actionRule);
+        }
+
+        // execute navigate back sequence
+        private void Navigate(ActionRule actionRule)
+        {
+            // set default value
+            var iterations = 1;
+
+            // normalize iterations
+            iterations = int.TryParse(actionRule.Argument, out int iterationsOut) ? iterationsOut : iterations;
+
+            // navigate
+            for (int i = 0; i < iterations; i++)
+            {
+                WebDriver.Navigate().Forward();
+                if (i >= iterations - 1)
+                {
+                    break;
+                }
+                WaitForState("complete", 200);
+            }
+        }
+    }
+}

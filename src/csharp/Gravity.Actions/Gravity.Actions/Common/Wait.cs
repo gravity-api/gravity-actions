ecd71ef238eda9b5d53a3d6d2978fc28bbacc10d
Gravity API <gravity.api@outlook.com>
2019-12-31 19:09:23 +0200

    Wait implemented

---
+++ src/csharp/Gravity.Actions/Gravity.Actions/Common/Wait.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * CHANGE LOG - keep only last 5 threads
+ * 
+ * 2019-01-12
+ *    - modify: improve XML comments
+ *    - modify: override ActionName using ActionType constant
+ *    
+ * 2019-12-31
+ *    - modify: add constructor to override base class types
+ * 
+ * on-line resources
+ */
+using Gravity.Services.Comet.Engine.Attributes;
+using Gravity.Services.Comet.Engine.Extensions;
+using Gravity.Services.Comet.Engine.Plugins;
+using Gravity.Services.DataContracts;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Gravity.Services.ActionPlugins.Common
+{
+    [Action(
+        assmebly: "Gravity.Services.ActionPlugins, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+        resource: "Gravity.Services.ActionPlugins.Documentation.wait.json",
+        Name = ActionType.WAIT)]
+    public class Wait : ActionPlugin
+    {
+        /// <summary>
+        /// Creates a new instance of this plug-in.
+        /// </summary>
+        /// <param name="webDriver">WebDriver implementation by which to execute the action.</param>
+        /// <param name="webAutomation">This WebAutomation object (the original object sent by the user).</param>
+        public Wait(IWebDriver webDriver, WebAutomation webAutomation)
+            : this(webDriver, webAutomation, Utilities.GetTypes())
+        { }
+
+        /// <summary>
+        /// Creates a new instance of this plug-in.
+        /// </summary>
+        /// <param name="webDriver">WebDriver implementation by which to execute the action.</param>
+        /// <param name="webAutomation">This WebAutomation object (the original object sent by the user).</param>
+        /// <param name="types">Types from which to load plug-ins repositories.</param>
+        public Wait(IWebDriver webDriver, WebAutomation webAutomation, IEnumerable<Type> types)
+            : base(webDriver, webAutomation, types)
+        { }
+
+        /// <summary>
+        /// clicks the mouse on the specified element
+        /// </summary>
+        /// <param name="actionRule">This ActionRule instance (the original object sent by the user).</param>
+        public override void OnPerform(ActionRule actionRule)
+        {
+            DoWait(actionRule);
+        }
+
+        /// <summary>
+        /// clicks the mouse on the specified element
+        /// </summary>
+        /// <param name="webElement">This WebElement instance on which to perform the action (provided by the extraction rule).</param>
+        /// <param name="actionRule">This ActionRule instance (the original object send by the user).</param>
+        public override void OnPerform(IWebElement webElement, ActionRule actionRule)
+        {
+            DoWait(actionRule);
+        }
+
+        // executes Wait routine
+        private void DoWait(ActionRule actionRule)
+        {
+            // parse waiting time
+            var isNumber = int.TryParse(actionRule.Argument, out int numberOut);
+            var isTimeSp = TimeSpan.TryParse(actionRule.Argument, out TimeSpan timespanOut);
+
+            // number handling
+            if (isNumber)
+            {
+                Thread.Sleep(numberOut);
+            }
+
+            // timespan handling
+            if (isTimeSp && !isNumber)
+            {
+                Thread.Sleep(timespanOut);
+            }
+        }
+    }
+}

a0909ad4a52aac29da980f74a2ae6cae7f17140a
Gravity API <gravity.api@outlook.com>
2020-01-09 22:24:38 +0200

    Keyboard implementation

---
+++ src/csharp/Gravity.Actions/Gravity.Actions/Web/Keyboard.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * CHANGE LOG - keep only last 5 threads
+ * 
+ * on-line resources
+ */
+using Gravity.Drivers.Selenium;
+using Gravity.Services.Comet.Engine.Attributes;
+using Gravity.Services.Comet.Engine.Extensions;
+using Gravity.Services.Comet.Engine.Plugins;
+using Gravity.Services.DataContracts;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+
+namespace Gravity.Services.ActionPlugins.Web
+{
+    [Action(
+        assmebly: "Gravity.Services.ActionPlugins, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+        resource: "Gravity.Services.ActionPlugins.Documentation.keyboard.json",
+        Name = ActionType.Keyboard)]
+    public class Keyboard : ActionPlugin
+    {
+        /// <summary>
+        /// Creates a new instance of this plug-in.
+        /// </summary>
+        /// <param name="webDriver">WebDriver implementation by which to execute the action.</param>
+        /// <param name="webAutomation">This WebAutomation object (the original object sent by the user).</param>
+        public Keyboard(IWebDriver webDriver, WebAutomation webAutomation)
+            : this(webDriver, webAutomation, Utilities.GetTypes())
+        { }
+
+        /// <summary>
+        /// Creates a new instance of this plug-in.
+        /// </summary>
+        /// <param name="webDriver">WebDriver implementation by which to execute the action.</param>
+        /// <param name="webAutomation">This WebAutomation object (the original object sent by the user).</param>
+        /// <param name="types">Types from which to load plug-ins repositories.</param>
+        public Keyboard(IWebDriver webDriver, WebAutomation webAutomation, IEnumerable<Type> types)
+            : base(webDriver, webAutomation, types)
+        { }
+
+        /// <summary>
+        /// Press a keyboard key on the provided element.
+        /// </summary>
+        /// <param name="actionRule">This ActionRule instance (the original object sent by the user).</param>
+        public override void OnPerform(ActionRule actionRule)
+        {
+            DoKeyboard(webElement: default, actionRule);
+        }
+
+        /// <summary>
+        /// Press a keyboard key on the provided element.
+        /// </summary>
+        /// <param name="webElement">This WebElement instance on which to perform the action (provided by the extraction rule).</param>
+        /// <param name="actionRule">This ActionRule instance (the original object sent by the user).</param>
+        public override void OnPerform(IWebElement webElement, ActionRule actionRule)
+        {
+            DoKeyboard(webElement, actionRule);
+        }
+
+        // executes Keyboard routine
+        private void DoKeyboard(IWebElement webElement, ActionRule actionRule)
+        {
+            // fetch locator
+            var by = ByFactory.Get(actionRule.Locator, actionRule.ElementToActOn);
+
+            // get element to act on
+            var element = webElement == default
+                ? WebDriver.GetElement(by, TimeSpan.FromMilliseconds(ElementSearchTimeout))
+                : webElement.FindElement(by);
+
+            // execute action
+            element.SendKeys(GetKey(actionRule.Argument));
+        }
+    }
+}

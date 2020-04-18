fb60b8b87798de3d3e991950127f50e85f22be32
Gravity API <gravity.api@outlook.com>
2020-04-18 21:14:52 +0300

    browser stack client improvements

--- src/csharp/Gravity.Plugins.Actions/Gravity.Plugins.Actions.IntegrationTests/Base/BrowserStackClient.cs
+++ src/csharp/Gravity.Plugins.Actions/Gravity.Plugins.Actions.IntegrationTests/Base/BrowserStackClient.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Net;
@@ -43,6 +44,11 @@ namespace Gravity.Plugins.Actions.IntegrationTests.Base
         public TimeSpan AttemptsTimeout { get; set; } = TimeSpan.FromSeconds(30);
 
         /// <summary>
+        /// Gets or sets the time to wait between sending get requests.
+        /// </summary>
+        public TimeSpan RequestsInterval { get; set; } = TimeSpan.FromMilliseconds(100);
+
+        /// <summary>
         /// Gets or sets the contract <see cref="JsonSerializerSettings"/> for this client requests.
         /// </summary>
         public JsonSerializerSettings SerializerSettings { get; set; }
@@ -52,31 +58,65 @@ namespace Gravity.Plugins.Actions.IntegrationTests.Base
         /// </summary>
         /// <param name="session">Session id to update.</param>
         /// <param name="requestBody">Session information to update.</param>
-        public Task PutAsync(string session, object requestBody)
+        public async Task PutAsync(string session, object requestBody)
         {
             // setup
             var body = JsonConvert.SerializeObject(requestBody, settings: SerializerSettings);
             var content = new StringContent(content: body, Encoding.UTF8, mediaType: "application/json");
             var requestUri = string.Format(format: BrowserStackApiFormat, session);
 
+            // get response
+            var response = await SendAsync(() => client.GetAsync(requestUri)).ConfigureAwait(false);
+            var status = await GetStatusAsync(response).ConfigureAwait(false);
+
             // execute
-            return SendAsync(() => client.PutAsync(requestUri, content));
+            try
+            {
+                while (status.Equals("running", StringComparison.OrdinalIgnoreCase))
+                {
+                    response = await SendAsync(() => client.GetAsync(requestUri)).ConfigureAwait(false);
+                    status = await GetStatusAsync(response).ConfigureAwait(false);
+                    await Task.Delay(RequestsInterval).ConfigureAwait(false);
+                }
+                await SendAsync(() => client.PutAsync(requestUri, content)).ConfigureAwait(false);
+            }
+            catch (Exception e) when (e != null)
+            {
+                Console.WriteLine($"{e}");
+            }
         }
 
         /// <summary>
         /// You can delete a session on the server, using DELETE.
         /// </summary>
         /// <param name="session">Session id to delete</param>
-        public Task DeleteAsync(string session)
+        public async Task DeleteAsync(string session)
         {
             // setup
             var requestUri = string.Format(format: BrowserStackApiFormat, session);
 
-            // execute
-            return SendAsync(() => client.DeleteAsync(requestUri));
+            // get response
+            var response = await SendAsync(() => client.GetAsync(requestUri)).ConfigureAwait(false);
+            var status = await GetStatusAsync(response).ConfigureAwait(false);
+
+            // get status
+            try
+            {
+                while (status.Equals("running", StringComparison.OrdinalIgnoreCase))
+                {
+                    response = await SendAsync(() => client.GetAsync(requestUri)).ConfigureAwait(false);
+                    status = await GetStatusAsync(response).ConfigureAwait(false);
+                    await Task.Delay(RequestsInterval).ConfigureAwait(false);
+                }
+                await SendAsync(() => client.DeleteAsync(requestUri)).ConfigureAwait(false);
+            }
+            catch (Exception e) when (e != null)
+            {
+                Console.WriteLine($"{e}");
+            }
         }
 
-        private async Task SendAsync(Func<Task<HttpResponseMessage>> factory)
+        private async Task<HttpResponseMessage> SendAsync(Func<Task<HttpResponseMessage>> factory)
         {
             // setup
             var timeout = AttemptsTimeout.TotalSeconds;
@@ -85,15 +125,35 @@ namespace Gravity.Plugins.Actions.IntegrationTests.Base
 
             // send to server
             var interval = 0;
+            HttpResponseMessage response = null;
             while (interval < timeout)
             {
-                var response = await factory.Invoke().ConfigureAwait(false);
+                response = await factory.Invoke().ConfigureAwait(false);
                 if (response.IsSuccessStatusCode || response.StatusCode == HttpStatusCode.NotFound)
                 {
-                    return;
+                    return response;
                 }
                 interval += 1000;
                 await Task.Delay(millisecondsDelay: 1000).ConfigureAwait(false);
+            }
+
+            // result
+            return response;
+        }
+
+        private async Task<string> GetStatusAsync(HttpResponseMessage response)
+        {
+            // get response body
+            var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            // parse for results
+            try
+            {
+                return JToken.Parse(responseBody)["automation_session"]["status"].ToString();
+            }
+            catch (Exception e) when (e != null)
+            {
+                return "running";
             }
         }
     }
---
+++ src/csharp/Gravity.Plugins.Actions/Gravity.Plugins.Actions.IntegrationTests/Suites.UiCommon/ConditionUrlTests.cs
@@ -0,0 +1,242 @@
+﻿/*
+ * CHANGE LOG - keep only last 5 threads
+ * 
+ * on-line resources
+ */
+using Gravity.Plugins.Actions.IntegrationTests.Base;
+using Gravity.Plugins.Actions.IntegrationTests.Cases.UiCommon.AssertScenarios;
+using Gravity.Plugins.Actions.IntegrationTests.Providers;
+using NUnit.Framework;
+
+using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
+
+namespace Gravity.Plugins.Actions.IntegrationTests.Suites.UiCommon
+{
+    [TestFixture]
+    public class ConditionUrlTests
+    {
+        //[Description(description: "P - [0045] - Assert, URL, Equal")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.Compatibilities))]
+        //public void T0045P(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = false;
+
+        //    // execute
+        //    var actual = new C0045().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "N - [0045] - Assert, URL, Equal")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.CompatibilitiesChrome))]
+        //public void T0045N(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = true;
+
+        //    // execute
+        //    var actual = new C0045().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "P - [0046] - Assert, URL, Not Equal")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.Compatibilities))]
+        //public void T0046P(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = false;
+
+        //    // execute
+        //    var actual = new C0046().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "N - [0046] - Assert, URL, Not Equal")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.CompatibilitiesChrome))]
+        //public void T0046N(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = true;
+
+        //    // execute
+        //    var actual = new C0046().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "P - [0047] - Assert, URL, Match")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.Compatibilities))]
+        //public void T0047P(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = false;
+
+        //    // execute
+        //    var actual = new C0047().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "N - [0047] - Assert, URL, Match")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.CompatibilitiesChrome))]
+        //public void T0047N(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = true;
+
+        //    // execute
+        //    var actual = new C0047().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "P - [0048] - Assert, URL, Not Match")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.Compatibilities))]
+        //public void T0048P(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = false;
+
+        //    // execute
+        //    var actual = new C0048().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "N - [0048] - Assert, URL, Not Match")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.CompatibilitiesChrome))]
+        //public void T0048N(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = true;
+
+        //    // execute
+        //    var actual = new C0048().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "P - [0049] - Assert, URL, Greater Than, Regular Expression")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.Compatibilities))]
+        //public void T0049P(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = false;
+
+        //    // execute
+        //    var actual = new C0049().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "N - [0049] - Assert, URL, Greater Than, Regular Expression")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.CompatibilitiesChrome))]
+        //public void T0049N(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = true;
+
+        //    // execute
+        //    var actual = new C0049().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "P - [0050] - Assert, URL, Lower Than, Regular Expression")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.Compatibilities))]
+        //public void T0050P(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = false;
+
+        //    // execute
+        //    var actual = new C0050().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "N - [0050] - Assert, URL, Lower Than, Regular Expression")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.CompatibilitiesChrome))]
+        //public void T0050N(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = true;
+
+        //    // execute
+        //    var actual = new C0050().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "P - [0051] - Assert, URL, Greater or Equal, Regular Expression")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.Compatibilities))]
+        //public void T0051P(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = false;
+
+        //    // execute
+        //    var actual = new C0051().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "N - [0051] - Assert, URL, Greater or Equal, Regular Expression")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.CompatibilitiesChrome))]
+        //public void T0051N(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = true;
+
+        //    // execute
+        //    var actual = new C0051().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "P - [0052] - Assert, URL, Lower or Equal, Regular Expression")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.Compatibilities))]
+        //public void T0052P(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = false;
+
+        //    // execute
+        //    var actual = new C0052().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+
+        //[Description(description: "N - [0052] - Assert, URL, Lower or Equal, Regular Expression")]
+        //[Test, TestCaseSource(typeof(CompatibilityProvider), nameof(CompatibilityProvider.CompatibilitiesChrome))]
+        //public void T0052N(AutomationEnvironment environment)
+        //{
+        //    // setup
+        //    environment.TestParams["negative"] = true;
+
+        //    // execute
+        //    var actual = new C0052().AddEnvironments(environment).Execute();
+
+        //    // assertion
+        //    Assert.IsTrue(actual);
+        //}
+    }
+}
